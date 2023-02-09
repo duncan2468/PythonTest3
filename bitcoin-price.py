715e5b6fac8bdac9cbab27fd09a9be83298216b1
duncandemo-admin <dwjcarson@gmail.com>
2023-02-09 12:15:47 +0000

    working version. Error message does not prevent functionality

--- bitcoin-price.py
+++ bitcoin-price.py
@@ -1,13 +1,12 @@
-import requests
 import json
-import os
+import requests
 from win10toast import ToastNotifier
 import time
 
-while True:
-    t = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=GBP").text
-    t = json.loads(t)
-    price = str(t["bitcoin"]["gbp"])
-    toast = ToastNotifier()
-    toast.show_toast("Bitcoin price", price, duration=5)
-    time.sleep(20)
+t = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp").text
+t = json.loads(t)
+price = str(t["bitcoin"]["gbp"])
+
+toast = ToastNotifier()
+toast.show_toast("Bitcoin price", price, duration = 5)
+time.sleep(20)
